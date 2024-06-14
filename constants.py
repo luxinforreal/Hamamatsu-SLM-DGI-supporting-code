cc7126ef597d389e4375859355f20768e8af0337
luxinforreal <luxinforreal@gmail.com>
2024-06-14 16:37:25 +0800

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,6 @@
+class Constants:
+    DLL_PATH = "C:/Users/allen/Desktop/Hamamatsu-SLM-DGI-supporting-code/Image_Control.dll"
+    TEST_BMP_PATH_SINGLE = "D:/Github/Hamamatsu-SLM-DGI-supporting-code/test/test-folder-1-bmp"
+    TEST_BMP_PATH_MULTIPLE = "D:/Github/Hamamatsu-SLM-DGI-supporting-code/test/test-folder-5-bmps"
+    BMP_ORIGIN_PATH = "D:/Speckle Patterns/Mathiue"
+    NPY_STORED_PATH = "D:/Speckle Patterns/Mathiue-npy"
