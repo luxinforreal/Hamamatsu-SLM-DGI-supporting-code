d4f06410667c176414104de6657cb4ba4b6de0a1
luxinforreal <luxinforreal@gmail.com>
2024-06-14 17:01:58 +0800

    Update constants.py

--- constants.py
+++ constants.py
@@ -3,4 +3,4 @@ class Constants:
     TEST_BMP_PATH_SINGLE = "D:/Github/Hamamatsu-SLM-DGI-supporting-code/test/test-folder-1-bmp"
     TEST_BMP_PATH_MULTIPLE = "D:/Github/Hamamatsu-SLM-DGI-supporting-code/test/test-folder-5-bmps"
     BMP_ORIGIN_PATH = "D:/Speckle Patterns/Mathiue"
-    NPY_STORED_PATH = "D:/Speckle Patterns/Mathiue-npy"
+    NPY_STORED_PATH = "D:/Speckle Patterns/Mathiue-npy/xxx.npy"  # 这里必须指定具体的npy文件
