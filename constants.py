93f3d4619729a755b60161e98317b0d90c5e0d9d
lux <54798029+luxinforreal@users.noreply.github.com>
2024-06-17 01:00:22 +0800

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,12 @@
+class Constants:
+    DLL_PATH = "C:/Users/allen/Desktop/Hamamatsu-SLM-DGI-supporting-code/Image_Control.dll"
+    TEST_BMP_PATH_SINGLE = "C:/Users/allen/Desktop/Hamamatsu-SLM-DGI-supporting-code/test/test-folder-1-bmp"
+    TEST_PNG_PATH_SINGLE = "C:/Users/allen/Desktop/Hamamatsu-SLM-DGI-supporting-code/test/test-folder-1-png"
+    TEST_BMP_PATH_MULTIPLE = "C:/Users/allen/Desktop/Hamamatsu-SLM-DGI-supporting-code/test/test-folder-5-bmps"
+    TEST_PNG_PATH_MULTIPLE = "C:/Users/allen/Desktop/Hamamatsu-SLM-DGI-supporting-code/test/test-folder-10-pngs"
+    TEST_MATHIUE_MULTIPLE = "C:/Users/allen/Desktop/Hamamatsu-SLM-DGI-supporting-code/test/test-folder-Mathiue-10-bmps"
+    BMP_ORIGIN_PATH = "D:/Speckle Patterns/Mathiue"
+    NPY_STORED_PATH = "C:/Users/allen/Desktop/Hamamatsu-SLM-DGI-supporting-code/test/test-folder-npy/test.npy"  # 这里必须指定具体的npy文件   
+    BESSEL_BMP_PATH = "D:/Speckle pattern/Bessel speckle pattern/BesselM0-1J/bmp/BesselM0J-50-bmp"   #BesselM0J-50-bmp/60/70 - M1J-10/30/40/60/70/80/90/100
+    MATHIUE_BMP_PATH = "D:/Speckle pattern/Mathiue-1280x1024-Compressed"
+    
